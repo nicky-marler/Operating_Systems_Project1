437b492c4e2289307f2adf7dd84eccbcae00c4e6
Nicholas Marler <nmarler@outlook.com>
2017-10-24 00:47:25 -0500

    Fixed comments

--- Operating_Systems_Project1/Project1.c
+++ Operating_Systems_Project1/Project1.c
@@ -7,24 +7,24 @@
 #define SUCCESS 1
 #define CHILD_PROCESSES 8
 
-//This function is to be called when a new process is to be made. It takes in a counter and substracts by one
+/*This function is to be called when a new process is to be made. It takes in a counter and substracts by one*/
 int make_process(int count) {
 
-	count--; //Counting down by one
-	int pid = fork(); //Creating a new process
+	count--; /*Counting down by one*/
+	int pid = fork(); /*Creating a new process*/
 
-	//If making a new process fails
+	/*If making a new process fails*/
 	if (pid < 0) {
 		perror("for failed");
 		exit(FAILURE);
 	}
-	//The newly parent process waiting on its child to finsih
+	/*The newly parent process waiting on its child to finsih*/
 	else if (pid > 0) {
 		/*parent*/
 		int status;
 		(void)waitpid(pid, &status, 0);
 	}
-	//Printing the child PID and making a new process if the counter is above 0
+	/*Printing the child PID and making a new process if the counter is above 0*/
 	else {
 		/*child*/
 		printf("Hello PID %ld\n", (long)getpid());
@@ -37,7 +37,7 @@ int make_process(int count) {
 	return SUCCESS;
 }
 
-//The main function that starts the recurssive program
+/*The main function that starts the recurssive program*/
 void main(void) {
 	make_process(CHILD_PROCESSES);
 }
